80fc3bd963f4a79c46b3b43cd2036601bd5e1cce
esgodra <danielpz.dp@gmail.com>
2022-09-17 16:22:16 -0600

    Feature: added login

--- controller/password.py
+++ controller/password.py
@@ -5,6 +5,8 @@ from flask import current_app
 from utils.config import http_error_dict
 from validator import validate
 from utils.environment import get_environment
+from model.user import get_user_data
+
 serverConfig = get_environment("Server")
 # Obtaining token api key
 pass_data = Blueprint("pass_data", __name__)
@@ -22,16 +24,44 @@ def get_user_by_password():
 
         if validate(data, rules):
             # Getting results from the request
-            sql_data = {
-                "data" : [],
-                "status" : "Success",
-                "detail" : ""
-            }
-            code = 200
-            return jsonify(sql_data), code
+            mongo_data, code = db_login(data)
+            return jsonify(mongo_data), code
         else:
             abort(400)
 
     except Exception as e:
         logger.info(f"Response={e}")
         abort(http_error_dict[type(e).__name__])
+
+def db_login(request_data):
+    response = {}
+    try:
+        username = request_data["user"]
+        password = request_data["password"]
+
+        array_user = get_user_data(username)
+        if len(array_user) > 0:
+            user = array_user[0]
+            pass_user = user["password"]
+
+            pass_bytes = bytes(password, encoding="utf-8")
+            pass_user_bytes = bytes(pass_user, encoding="utf-8")
+            if bcrypt.checkpw(pass_bytes, pass_user_bytes):
+                array_user[0].pop("password")
+                data = array_user
+                status = "Success"
+                code = 200
+            else:
+                detail = "Incorrect pin"
+                data = ""
+
+        else:
+            detail = "No operator with the phone number was found."
+    except Exception as e:
+        status = "Error"
+        detail = str(e)
+
+    response["data"] = data
+    response["status"] = status
+    response["detail"] = detail
+    return response
